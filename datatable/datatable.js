827f96226aef43539a23dfaf6a03ad19bcc289fe
imtiazahmed80 <ia64744@gmail.com>
2023-06-20 23:12:14 +0600

    119th commit

--- datatable/datatable.js
+++ datatable/datatable.js
@@ -10,21 +10,16 @@ function  miDataTable(){
 
       "language": {
       "emptyTable":			"<i>No hay datos disponibles en la tabla.</i>",
-      "info":		   		"Del _START_ al _END_ de _TOTAL_ ",
       "infoEmpty":			"Mostrando 0 registros de un total de 0.",
       "infoFiltered":			"(filtrados de un total de _MAX_ registros)",
-      "infoPostFix":			"(actualizados)",
-      "lengthMenu":			"Mostrar _MENU_ registros",
       "loadingRecords":		"Cargando...",
       "processing":			"Procesando...",
-      "search":			"<span style='font-size:15px;'>Buscar:</span>",
-      "searchPlaceholder":		"Dato para buscar",
       "zeroRecords":			"No se han encontrado coincidencias.",
       "paginate": {
         "first":			"Primera",
         "last":				"Última",
-        "next":				"Siguiente",
-        "previous":			"Anterior"
+        "next":				"Next",
+        "previous":			"Prev"
       },
       "aria": {
         "sortAscending":	"Ordenación ascendente",
@@ -32,8 +27,8 @@ function  miDataTable(){
       }
     },
 
-    "lengthMenu":		[[3,5,7, 10, 20, 25, 50, -1], [3,5,7, 10, 20, 25, 50, "Todos"]],
-    "iDisplayLength":	3,
+
+    "iDisplayLength":	10,
 
 
 
