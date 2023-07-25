4bec9b7d524d85b2f9ddc5f2d588f11e0716c644
rahatcnits <rahatcnits@gmail.com>
2023-07-25 12:37:34 +0600

    148th commit

--- datatable/datatable.js
+++ datatable/datatable.js
@@ -1,38 +1,28 @@
-$(document).ready(function() {
-miDataTable();
-} );
+$(document).ready(function () {
+  miDataTable();
+});
 
-
-
-
-function  miDataTable(){
-    $('#miTabla').DataTable({
-
-      "language": {
-      "emptyTable":			"<i>No hay datos disponibles en la tabla.</i>",
-      "infoEmpty":			"Mostrando 0 registros de un total de 0.",
-      "infoFiltered":			"(filtrados de un total de _MAX_ registros)",
-      "loadingRecords":		"Cargando...",
-      "processing":			"Procesando...",
-      "zeroRecords":			"No se han encontrado coincidencias.",
-      "paginate": {
-        "first":			"Primera",
-        "last":				"Última",
-        "next":				"Next",
-        "previous":			"Prev"
+function miDataTable() {
+  $("#miTabla").DataTable({
+    language: {
+      emptyTable: "<i>No hay datos disponibles en la tabla.</i>",
+      infoEmpty: "Mostrando 0 registros de un total de 0.",
+      infoFiltered: "(filtrados de un total de _MAX_ registros)",
+      loadingRecords: "Cargando...",
+      processing: "Procesando...",
+      zeroRecords: "No se han encontrado coincidencias.",
+      paginate: {
+        first: "Primera",
+        last: "Última",
+        next: "Next",
+        previous: "Prev",
       },
-      "aria": {
-        "sortAscending":	"Ordenación ascendente",
-        "sortDescending":	"Ordenación descendente"
-      }
+      aria: {
+        sortAscending: "Ordenación ascendente",
+        sortDescending: "Ordenación descendente",
+      },
     },
 
-
-    "iDisplayLength":	10,
-
-
-
-
-
-    });
+    iDisplayLength: 10,
+  });
 }
